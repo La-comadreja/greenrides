efc86a51df51b9ee108f802bd972db8bf32d6bab
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-10-26 14:01:20 -0400

    Passengers table is Devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131026171345) do
+ActiveRecord::Schema.define(version: 20131026174828) do
 
   create_table "cars", force: true do |t|
     t.string   "origin_city"
@@ -26,8 +26,17 @@ ActiveRecord::Schema.define(version: 20131026171345) do
   end
 
   create_table "passengers", force: true do |t|
-    t.string   "name"
     t.string   "email"
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "name"
     t.string   "university"
     t.integer  "year"
     t.string   "department"
@@ -35,5 +44,8 @@ ActiveRecord::Schema.define(version: 20131026171345) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "passengers", ["email"], name: "index_passengers_on_email", unique: true, using: :btree
+  add_index "passengers", ["reset_password_token"], name: "index_passengers_on_reset_password_token", unique: true, using: :btree
 
 end
