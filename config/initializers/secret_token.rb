1621f769b39b1d8ef74709e84b0d541b6478fdf4
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-10-26 12:21:15 -0400

    Create new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Greenrides::Application.config.secret_key_base = 'ed716546561adbc2609aafcf6945d59b673d61e0bd913d9ddc996094a4c3bd800c786d382392bd649f8eda2b05ad14c69677361b809fa84ff32d87e1c06f50e4'
