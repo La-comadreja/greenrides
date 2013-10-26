9c8fa9b90977bcb51f1a192204e94cb98b3c883c
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-10-26 13:14:19 -0400

    Create passengers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131026170054) do
+ActiveRecord::Schema.define(version: 20131026171345) do
 
   create_table "cars", force: true do |t|
     t.string   "origin_city"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 20131026170054) do
     t.datetime "departure"
     t.integer  "seats"
     t.decimal  "payment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "passengers", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "university"
+    t.integer  "year"
+    t.string   "department"
+    t.string   "interests"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
